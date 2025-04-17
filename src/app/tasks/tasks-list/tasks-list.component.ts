4d8bbb9001caa461cd5de0e2e8d83397b1bdc836
Luis Henrique <luishng@gmail.com>
2025-04-17 14:26:04 -0300

    feat: Add first service: tasksService in app

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-tasks-list',
@@ -10,8 +11,21 @@ import { TaskItemComponent } from './task-item/task-item.component';
   imports: [TaskItemComponent],
 })
 export class TasksListComponent {
-  selectedFilter = signal<string>('all');
-  tasks = [];
+  private selectedFilter = signal<string>('all');
+  private tasksService = inject(TasksService)
+
+  tasks = computed(() => {
+    switch (this.selectedFilter()) {
+      case 'open':
+        return this.tasksService.allTasks().filter(task => task.status === 'OPEN')
+      case 'in-progress':
+        return this.tasksService.allTasks().filter(task => task.status === 'IN_PROGRESS')
+      case 'done':
+        return this.tasksService.allTasks().filter(task => task.status === 'DONE')
+      default:
+        return this.tasksService.allTasks()
+    }
+  })
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
