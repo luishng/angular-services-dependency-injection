6f0c55d54f48a721b40380fa8515d2275b3f0afa
Luis Henrique <luishng@gmail.com>
2025-04-17 14:44:20 -0300

    feat: add log services to show how injects works

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,11 +1,13 @@
-import { Injectable, signal } from "@angular/core";
+import { inject, Injectable, signal } from "@angular/core";
 import { Task, TaskStatus } from "./task.model";
+import { LoggingService } from "../logging.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
   private tasks = signal<Task[]>([]);
+  private logService = inject(LoggingService)
 
   allTasks = this.tasks.asReadonly();
 
@@ -17,9 +19,11 @@ export class TasksService {
     }
 
     this.tasks.update((oldTasks) => [...oldTasks, newTask])
+    this.logService.log('Add task with the title:' + taskData.title)
   }
 
   updateTaskStatus(taskId: string, newStatus: TaskStatus) {
     this.tasks.update((oldTasks) => oldTasks.map((task) => task.id === taskId ? { ...task, status: newStatus } : task))
+    this.logService.log('Change status status to:' + newStatus)
   }
 }
